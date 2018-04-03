a884ccc8669e0dbcc5ab64586b3e67f5a78cae55
David Passlow <s171097@students.aie.edu.au>
2018-04-03 13:19:40 +1000

    clock implementation pass half done

--- RhythmLib/GameClock.cpp
+++ RhythmLib/GameClock.cpp
@@ -4,9 +4,35 @@
 
 GameClock::GameClock()
 {
+	m_createTime = m_timer.now();
+	m_lastTime = m_createTime;
 }
 
 
 GameClock::~GameClock()
 {
 }
+
+int64_t GameClock::getTimeSeconds()
+{
+	return std::chrono::duration_cast<std::chrono::seconds>(m_timer.now() - m_createTime).count();
+}
+
+int64_t GameClock::getTimeMilliseconds()
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds>(m_timer.now() - m_createTime).count();
+}
+
+int64_t GameClock::getTimeMicroseconds()
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(m_timer.now() - m_createTime).count();
+}
+
+void GameClock::update()
+{
+	deltaTime = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(m_timer.now() - m_lastTime).count() / 1000.0f;
+	m_lastTime = m_timer.now();
+}
+
+
+
--- RhythmLib/GameClock.h
+++ RhythmLib/GameClock.h
@@ -1,15 +1,31 @@
 #pragma once
+#include <chrono>
+
+//class basically copied from andrew https://github.com/andrewthenintendrone
 class GameClock
 {
 public:
 	GameClock();
 	~GameClock();
 
+	int64_t getTimeSeconds();
+	int64_t getTimeMilliseconds();
+	int64_t getTimeMicroseconds();
 
+	void update();
 
+	float getDeltaTime();
 
+private:
 
+	std::chrono::high_resolution_clock m_timer;
 
+	//the time that this clock was created at
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_createTime;
+
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_lastTime;
+
+	float deltaTime = 0.0f;
 
 };
 
