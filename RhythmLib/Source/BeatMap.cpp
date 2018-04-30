d8044b6e8d19016532810ebbe451be63e17bb1ec
Darklight437 <darklight437@gmail.com>
2018-04-30 15:08:46 +1000

    converting away from "events" architechture

--- RhythmLib/Source/BeatMap.cpp
+++ RhythmLib/Source/BeatMap.cpp
@@ -8,17 +8,17 @@ BeatMap::~BeatMap()
 {
 }
 
-std::vector<eventpoint> BeatMap::getTimeEvents()
+std::vector<int64_t> BeatMap::getTimeEvents()
 {
-	return std::vector<eventpoint>();
+	return std::vector<int64_t>();
 }
 
 void BeatMap::recordEvent()
 {
-	eventpoint beat;
-	beat.timeEvent = GameClock::getInstance().getTimeMilliseconds();
+	int64_t beat;
+	beat = GameClock::getInstance().getTimeMilliseconds();
 
-		beatEvents.push_back(beat);
+	beatTimes.push_back(beat);
 }
 
 
